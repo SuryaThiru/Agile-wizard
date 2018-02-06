63d81efb3309259ad8addd41c58862a1315f6896
Surya <surya.thiru001@gmail.com>
2018-02-07 00:41:37 +0530

    refactoring continues...

--- graphql/schema.js
+++ graphql/schema.js
@@ -10,11 +10,10 @@ const message = {
     to: 'amrut546@gmail.com',
     from: 'onimusha702@gmail.com',
     subject: 'Hey buddy!',
-    text: 'is this working',
-    html: '<strong>Html test</strong>'
+    text: 'it is working',
+    html: '<strong>WORKING MFs!!!</strong>'
 };
 const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env['sendgrid']);
 
 const {
   GraphQLString,
@@ -24,7 +23,8 @@ const {
 
 const {
   registerResponse,
-  authResponse
+  authResponse,
+  queryResponse
 } = require('./types');
 
 const {
@@ -37,18 +37,23 @@ const {
   formatErrors
 } = require('./utils');
 
-let serviceaccount = require('../config/skindoc-10ef5-firebase-adminsdk-hye37-c5e3f153c1');
-admin.initializeApp({
+const serviceaccount = require('../config/skindoc-10ef5-firebase-adminsdk-hye37-c5e3f153c1');
+const mailkey = require('../config/mail_api').key;
+
+admin.initializeApp ({
   credential: admin.credential.cert(serviceaccount),
   databaseURL: "https://skindoc-10ef5.firebaseio.com"
 });
+
+sgMail.setApiKey(mailkey);
+
 let db = admin.firestore();
 
 let queryType = new GraphQLObjectType({
   name: 'Query',
   fields:{
     findUser:{
-      type: registerResponse,
+      type: queryResponse,
       args: {
         viewer: {type: viewerInput }
       },
@@ -60,8 +65,10 @@ let queryType = new GraphQLObjectType({
             errors: "Token is invalid."
           }
         }
+
         let {token} = something.viewer;
-        return jwt.verify(token, process.env['jwt_secret'], (err, decoded)=>{
+
+        return jwt.verify(token, 'secret', (err, decoded) => {
           if(err) {
             let message = formatErrors(err);
             return {
@@ -70,8 +77,10 @@ let queryType = new GraphQLObjectType({
               errors: message
             }
           }
+
           let {email} = decoded;
-          return db.collection("users").doc(email).get().then((doc)=>{
+
+          return db.collection("users").doc(email).get().then((doc) => {
             if(!doc.exists){
               return{
                 flag: false,
@@ -110,31 +119,29 @@ let mutationType = new GraphQLObjectType({
         let users = db.collection('users').doc(params.input.email);
         let userData = JSON.parse(JSON.stringify(params.input));
 
-        return users.create(userData).then(()=>{
-          return test(params.input);
-        }).then((obj)=>{
-          console.log(obj);
-          return sgMail.send(message).then(()=>{
-            function omega(){
-              return obj;
-            }
-            return omega();
-          }).then((dat)=>{
-            return dat;
-          }).catch((err)=>{
-            console.log('inside catch 1');
+        return users.create(userData)
+          .then(() => {
+          // return test(params.input);
+            sgMail.send(message)
+              .then(() => {
+                console.log('mail sent!');
+              })
+              .catch(err => {
+                console.log('error: ' + err);
+              });
+
+          return {
+            flag: true,
+            token: jwt.sign({email: userData.email}, 'secret'),
+            errors: null
+          };
+        })
+          .catch((err) => {
             return {
               flag: false,
-              token: false,
+              token: null,
               errors: formatErrors(err)
             }
-          });
-        }).catch((err)=>{
-          return {
-            flag: false,
-            token: null,
-            errors: formatErrors(err)
-          }
         });
       }
     },
@@ -144,53 +151,60 @@ let mutationType = new GraphQLObjectType({
         email: {type: GraphQLString},
         password: {type: GraphQLString}
       },
-      resolve: (root, params)=>{
+      resolve: (root, params) => {
         let {password, email} = params;
-        return db.collection("users").doc(email).get().then((doc)=>{
-          if(!doc.exists)
-            return false;
-          else
-            return doc.data();
-        }).then((dat)=>{
-          console.log(dat);
-          if(!dat){
-            return{
-              flag: true,
-              errors: "Invalid username",
-              user: null,
-              token: null
-            }
-          }
-          else {
-            return bcrypt.compare(password, dat.password).then((res) => {
-              if (res) {
-                return {
-                  flag: true,
-                  errors: null,
-                  user: dat,
-                  token: jwt.sign({email: email}, process.env['jwt_secret'])
-                }
-              }
-              else {
-                return {
-                  flag: false,
-                  errors: "Invalid Password.",
-                  user: dat,
-                  token: null
-                }
-              }
-            }).catch(err => {
+
+        return db.collection("users").doc(email).get()
+          .then((doc)=>{
+            if(!doc.exists) {
               return {
-                flag: false,
-                errors: err.message,
-                user: dat,
+                flag: true,
+                errors: "Invalid username",
+                user: null,
                 token: null
               }
-            });
-          }
-        }).catch((err)=>{
-          console.log(err);
-        });
+            }
+            else {
+              let dat = doc.data();
+
+              // compares password
+              return bcrypt.compare(password, dat.password)
+                .then((res) => {
+                  if (res) {
+                    return {
+                      flag: true,
+                      errors: null,
+                      user: dat,
+                      token: jwt.sign({email: email}, 'secret')
+                    }
+                  }
+                  else {
+                    return {
+                      flag: false,
+                      errors: "Invalid Password.",
+                      user: dat,
+                      token: null
+                    }
+                  }
+                })
+                .then(dat => {
+                  console.log(dat);
+                  return dat;
+                })
+                .catch(err => {
+                  return {
+                    flag: false,
+                    errors: err.message,
+                    user: null,
+                    token: null
+                  }
+                });
+            }
+
+          })
+          .catch((err)=>{
+            console.log(err);
+          });
       }
     }
   }
--- graphql/utils.js
+++ graphql/utils.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+
 const test = (stuff)=>{
   return {
     flag: true,
